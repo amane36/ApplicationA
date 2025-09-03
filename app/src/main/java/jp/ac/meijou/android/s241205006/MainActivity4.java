83d0cda2f95613c32aac803ec13744bb3e19c698
amane36 <241205006@ccmailg.meijo-u.ac.jp>
2025-09-03 15:02:34 +0900

    3限目終わったときにコミットし忘れた

---
+++ app/src/main/java/jp/ac/meijou/android/s241205006/MainActivity4.java
@@ -0,0 +1,29 @@
+package jp.ac.meijou.android.s241205006;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import jp.ac.meijou.android.s241205006.databinding.ActivityMain2Binding;
+import jp.ac.meijou.android.s241205006.databinding.ActivityMain4Binding;
+
+public class MainActivity4 extends AppCompatActivity {
+    private ActivityMain4Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMain4Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
