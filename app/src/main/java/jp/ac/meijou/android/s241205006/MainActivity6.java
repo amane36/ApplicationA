3d700838345c637e36f213575cdc40dea7694d54
amane36 <241205006@ccmailg.meijo-u.ac.jp>
2025-09-04 14:13:08 +0900

    画像を取得して表示する演習

--- app/src/main/java/jp/ac/meijou/android/s241205006/MainActivity6.java
+++ app/src/main/java/jp/ac/meijou/android/s241205006/MainActivity6.java
@@ -1,7 +1,10 @@
 package jp.ac.meijou.android.s241205006;
 
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -10,30 +13,78 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
 import java.io.IOException;
 
+import jp.ac.meijou.android.s241205006.databinding.ActivityMain5Binding;
+import jp.ac.meijou.android.s241205006.databinding.ActivityMain6Binding;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class MainActivity6 extends AppCompatActivity {
 
-    private MainActivity6 binding;
+    private ActivityMain6Binding binding;
+
+    private final OkHttpClient okHttpClient = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main6);
+        binding = ActivityMain6Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        TextView Loading = findViewById(R.id.loading);
+        ImageView ImageView2 = findViewById(R.id.imageView2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        binding.buttonget.setOnClickListener(view -> {
+            var text = binding.editTextText3.getText().toString();
+
+            // textパラメータをつけたURLの作成
+            var url = Uri.parse("https://placehold.jp/500x500.png")
+                    .buildUpon()
+                    .appendQueryParameter("text", text)
+                    .build()
+                    .toString();
+            ImageView2.setVisibility(ImageView.INVISIBLE);
+            Loading.setVisibility(TextView.VISIBLE);
+            getImage(url);
+        });
+    }
+    private void getImage(String url) {
+        // リクエスト先に画像URLを指定
         var request = new Request.Builder()
-                .url("https://placehold.jp/350x350.png")
+                .url(url)
                 .build();
+
+        // 非同期通信でリクエスト
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                // 通信に失敗した時に呼ばれる
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                // InputStreamをBitmapに変換
+                var bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                // UIスレッド以外で更新するとクラッシュするので、UIスレッド上で実行させる
+
+                runOnUiThread(() -> {
+                    binding.loading.setVisibility(TextView.GONE);
+                    binding.imageView2.setVisibility(ImageView.VISIBLE);
+                    binding.imageView2.setImageBitmap(bitmap);
+                });
+            }
+        });
     }
 }
